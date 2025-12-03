9b71673b988b189649c8a68a85dae2fda217a1bd
Daniel Cepeda <dcep93@gmail.com>
2025-12-03 01:51:16 -0500

    x

--- etc/advent_of_code/2025/03.py
+++ etc/advent_of_code/2025/03.py
@@ -2,7 +2,7 @@
 
 import pathlib
 
-use_example = True
+use_example = False
 
 with open(f"./{pathlib.Path(__file__).stem}.txt") as fh:
     txt = fh.read()
@@ -14,23 +14,65 @@ def get_lines():
 
 
 def main():
-    print(part1.run())
-    # assert use_example or part1.run() ==
-    # print(part2.run())
+    # print(part1.run(True))
+    assert use_example or part1.run() == 17766
+    print(part2.run())
 
 
 class part1:
+    should_log = False
+
+    @classmethod
+    def log(cls, str):
+        if cls.should_log:
+            print(str)
+
     @classmethod
     def run(cls):
-        return 0
+        return cls.run_size(2)
+
+    @classmethod
+    def run_size(cls, size):
+        total = 0
+        for _, line in get_lines():
+            besties = [int(b) for b in line[-size:]]
+            for c in reversed(line[:-size]):
+                v = int(c)
+                for i, b in enumerate(besties):
+                    if v < b:
+                        break
+                    if v > b:
+                        besties[i] = v
+                        v = b
+            vv = sum([10**i * b for i, b in enumerate(besties[::-1])])
+            total += vv
+        return total
+
+    @classmethod
+    def old(cls, line):
+        best = int(line[-2])
+        next_best = int(line[-1])
+        for c in reversed(line[:-2]):
+            v = int(c)
+            if v >= best:
+                next_best = max(best, next_best)
+                best = v
+        return 10 * best + next_best
 
 
 class part2(part1):
-    pass
+    should_log = True
+
+    @classmethod
+    def run(cls):
+        return cls.run_size(12)
 
 
 EXAMPLE = """
-
+987654321111111
+811111111111119
+234234234234278
+818181911112111
 """
 
 
---
+++ etc/advent_of_code/2025/12.py
@@ -0,0 +1,77 @@
+# cdr && cd dcep93.github.io/etc/advent_of_code/2025 && python3 12.py
+
+import hashlib
+import pathlib
+
+
+def main():
+    log_part_1 = part1.answer_md5 is None
+    part1.main(should_log=log_part_1, use_example=True)
+    if not log_part_1:
+        part2.main(should_log=True, use_example=True)
+
+
+class part1:
+    answer_md5 = None
+    should_log: bool
+    txt: str
+
+    @classmethod
+    def run_line(cls, index: int, line: str) -> int:
+        return 0
+
+    @classmethod
+    def main(cls, *, should_log, use_example):
+        cls.should_log = should_log
+
+        if use_example:
+            cls.txt = EXAMPLE[1:-1]
+        else:
+            with open(f"./{pathlib.Path(__file__).stem}.txt") as fh:
+                cls.txt = fh.read()
+
+        rval = cls.run()
+        rval_md5 = cls.md5(rval)
+        assert use_example or cls.answer_md5 is None or cls.answer_md5 == rval_md5
+        cls.log(rval)
+        cls.log(f"hash: {rval_md5}")
+
+    @classmethod
+    def run(cls):
+        total = 0
+        for i, line in cls.get_lines():
+            total += cls.run_line(i, line)
+        return total
+
+    @classmethod
+    def get_lines(cls):
+        for index, line in enumerate(cls.txt.split("\n")):
+            yield index, line
+
+    @classmethod
+    def log(cls, str):
+        if cls.should_log:
+            print(str)
+
+    @classmethod
+    def md5(cls, value):
+        num_str = str(value)
+        num_bytes = num_str.encode("utf-8")
+        m = hashlib.md5()
+        m.update(num_bytes)
+        return m.hexdigest()
+
+
+class part2(part1):
+
+    @classmethod
+    def run_line(cls):
+        return 0
+
+
+EXAMPLE = """
+
+"""
+
+
+main()
